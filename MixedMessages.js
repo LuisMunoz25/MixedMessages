ea0c8a7a06d2a56ce0e0fa277dee146d5f151b0e
Luis <luismunoz2592@gmail.com>
2024-04-22 14:04:31 -0400

    created collective messages

--- MixedMessages.js
+++ MixedMessages.js
@@ -1,1 +1,10 @@
-//
+//Get a number from 0 -> num - 1
+const generateRandomNumber = num => {
+    return Math.floor(Math.random() * num)
+}
+
+const collectiveWisdom = {
+    signInfo: ['star', 'moon', 'sun', 'comet'],
+    fortuneOutput: ['really bad luck', 'bad luck', 'ok luck', 'good luck', 'great luck'],
+    advice: ['go have ice cream', 'don\'t read this', 'play more', 'don\'t even trust your own shadow']
+}
